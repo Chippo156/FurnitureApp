0af8f3bbb05f4afdbc75694d7c55dabe8b50e54d
Chippo156 <vovannghiahiep150623@gmail.com>
2024-10-13 16:24:22 +0700

    update app cóntants

--- src/app/common/AppContants.ts
+++ src/app/common/AppContants.ts
@@ -1,13 +1,13 @@
 export class AppContants {
   private static redirect_uri = 'https://furniture-app-delta.vercel.app/';
   public static URL_GOOGLE =
-    'http://localhost:8088/oauth2/authorization/google' +
+    'https://furnitureapp-x2c8.onrender.com/oauth2/authorization/google' +
     '?redirect-uri=' +
     AppContants.redirect_uri;
   public static URL_FACEBOOK =
-    'http://localhost:8088/oauth2/authorization/facebook' +
+    'https://furnitureapp-x2c8.onrender.com/oauth2/authorization/facebook' +
     '?redirect-uri=' +
     AppContants.redirect_uri;
   public static URL_GITHUB =
-    'http://localhost:8088/oauth2/authorization/github';
+    'https://furnitureapp-x2c8.onrender.com/oauth2/authorization/github';
 }
