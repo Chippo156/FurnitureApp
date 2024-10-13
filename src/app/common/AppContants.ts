d1f06448cd2d7f3da799893f4f235bc39d848c1e
Chippo156 <vovannghiahiep150623@gmail.com>
2024-10-13 15:55:08 +0700

    update oauth2

--- src/app/common/AppContants.ts
+++ src/app/common/AppContants.ts
@@ -1,5 +1,5 @@
 export class AppContants {
-  private static redirect_uri = 'http://localhost:4200/';
+  private static redirect_uri = 'https://furniture-app-delta.vercel.app/';
   public static URL_GOOGLE =
     'http://localhost:8088/oauth2/authorization/google' +
     '?redirect-uri=' +
