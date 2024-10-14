239e7d44784f26894a8e539df8b41adec4b665e1
Chippo156 <vovannghiahiep150623@gmail.com>
2024-10-14 12:57:05 +0700

    get instance TIME

--- src/main/java/org/ecommerce/ecommerce/configurations/VnpayConfig.java
+++ src/main/java/org/ecommerce/ecommerce/configurations/VnpayConfig.java
@@ -44,7 +44,7 @@ public class VnpayConfig {
         vnPayParamsMap.put("vnp_OrderType", orderType);
         vnPayParamsMap.put("vnp_ReturnUrl", vnp_ReturnUrl);
 
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT+7"));
+        Calendar calendar = Calendar.getInstance();
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnp_CreateDate = formatter.format(calendar.getTime());
         vnPayParamsMap.put("vnp_CreateDate", vnp_CreateDate);
